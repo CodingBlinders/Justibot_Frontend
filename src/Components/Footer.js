ce80bc48996b3b5f96c1a8aa8479dbbe60e83471
isuruliyanage2001 <isuruliyanageofficial@gmail.com>
2023-12-17 01:03:44 +0530

    changes on ui

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="row py-3 mt-auto">
       <div className="col text-center">
-        <p className="text-light m-0">JustiBot 2023 &copy; All rights reserved</p>
+        <p className=" m-0 " style={{color:'#9b9b9b'}} >JustiBot 2023 &copy; All rights reserved</p>
       </div>
     </footer>
   );
