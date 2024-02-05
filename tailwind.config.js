3241d2ad316589934484737fe8132cfe986070b3
dhananjayaaps <dhananjayaaps@gmail.com>
2024-02-05 06:52:19 +0530

    made configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ module.exports = {
   content: [
     // ...
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./src/*/**.{js,ts,jsx,tsx}"
+    "./src/*/**.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui(), require('flowbite/plugin')],
 };
