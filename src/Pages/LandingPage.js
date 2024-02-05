8e9393045949922f256d0f40f00c302141711b4c
dhananjayaaps <dhananjayaaps@gmail.com>
2024-02-05 06:47:29 +0530

    made a change in the landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,21 +1,56 @@
 import React from 'react';
 import LadyOfJustice from '../Assets/Images/ladyofjustice-2.jpg';
 import {Button, ButtonGroup} from "@nextui-org/react";
+import { TypeAnimation } from 'react-type-animation';
 
 import {Link} from "@nextui-org/react";
 import NavBar from '../Components/NavBar';
+import { LinearGradient } from 'react-text-gradients'
+
+
+
+const ExampleComponent = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Explore legal intricacies seamlessly.',
+        2000,
+        'Explore law student simulations.',
+        2000,
+        'Explore real legal scenarios.',
+        2000,
+        'Explore precise business strategies.',
+        2000,
+      
+        'Explore the legal world.',
+        2000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' , alignContent: 'left'}}
+      repeat={Infinity}
+    />
+  );
+};
 
 
 const LandingPage = () => {
   return (
     <div>
-      <NavBar />
+    <NavBar />
     <div className="container mt-5">
       <div className="row align-items-center">
         <div className="col-md-6">
+          <h1 className="large-text">
+            <LinearGradient gradient={['to left', '#17acff ,#ff68f0']}>
+              Welcome to the JustiBot
+            </LinearGradient>
+          </h1><br/>
+
           <div className="text-center text-light">
-            <h1>Welcome to the JustiBot</h1>
-            <p>Chat now with Your Legal AI Assistant !</p>
+            <ExampleComponent/>
+            <p>Chat now with Your Legal AI Assistant !</p><br/>
             
             <Button color="primary" variant="bordered" text-colour="white">
               <Link to="/chat">
---
+++ src/Pages/login.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon
+}
+from 'mdb-react-ui-kit';
+
+function App() {
+  return (
+    <MDBContainer fluid>
+
+      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+        <MDBCol col='12'>
+
+          <MDBCard className='bg-dark text-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '400px'}}>
+            <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+
+              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+              <p className="text-white-50 mb-5">Please enter your login and password!</p>
+
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Email address' id='formControlLg' type='email' size="lg"/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Password' id='formControlLg' type='password' size="lg"/>
+
+              <p className="small mb-3 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+              <MDBBtn outline className='mx-2 px-5' color='white' size='lg'>
+                Login
+              </MDBBtn>
+
+              <div className='d-flex flex-row mt-3 mb-5'>
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                  <MDBIcon fab icon='facebook-f' size="lg"/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                  <MDBIcon fab icon='twitter' size="lg"/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                  <MDBIcon fab icon='google' size="lg"/>
+                </MDBBtn>
+              </div>
+
+              <div>
+                <p className="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a></p>
+
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+
+        </MDBCol>
+      </MDBRow>
+
+    </MDBContainer>
+  );
+}
+
+export default App;
