6ac283e98a996b65eb1176fdc91de8a79019e50a
dhananjayaaps <dhananjayaaps@gmail.com>
2024-02-10 03:51:21 +0530

    fixed chat bot.js

--- src/Components/ChatBot.js
+++ src/Components/ChatBot.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
-
 import Format from './Format';
 import logo from '../Assets/Images/justibot-only-logo.png';
-import {Button} from "@nextui-org/react";
-import {VscSend} from "react-icons/vsc";
-import { VscRefresh } from 'react-icons/vsc';
-
+import { VscSend, VscRefresh } from 'react-icons/vsc';
 
 const ChatBot = () => {
   const initialMessage = {
@@ -23,6 +19,7 @@ const ChatBot = () => {
   const [showIntro, setShowIntro] = useState(true);
   const [messages, setMessages] = useState([]);
   const [isGenerating, setIsGenerating] = useState(false);
+  const [userInput, setUserInput] = useState('');
 
   useEffect(() => {
     const savedMessages = JSON.parse(localStorage.getItem('chatMessages'));
@@ -31,17 +28,6 @@ const ChatBot = () => {
       setMessages(savedMessages);
     }
   }, []);
-
-  // useEffect(() => {
-  //   if (showIntro) {
-  //     setTimeout(() => {
-  //       setShowIntro(false);
-  //       setMessages([]);
-  //     }, 3000); // Change the duration as needed (e.g., 3000ms for 3 seconds)
-  //   }
-  // }, [showIntro]);
-
-  const [userInput, setUserInput] = useState('');
 
   const handleUserInput = (e) => {
     setUserInput(e.target.value);
@@ -76,7 +62,6 @@ const ChatBot = () => {
         const result = await response.text();
 
         setTimeout(() => {
-
           const botMessage = { text: result, isUser: false };
           const updatedMessagesWithBot = [...updatedMessages, botMessage];
           localStorage.setItem('chatMessages', JSON.stringify(updatedMessagesWithBot));
@@ -90,71 +75,40 @@ const ChatBot = () => {
     }
   };
 
-  // const handleSubmitHard = (e) => {
-  //   e.preventDefault();
-  //   if (userInput.trim() !== '') {
-  //     const userMessage = { text: userInput, isUser: true };
-
-  //     const updatedMessages = [...messages, userMessage];
-  //     localStorage.setItem('chatMessages', JSON.stringify(updatedMessages));
-  //     setMessages(updatedMessages);
-  //     setUserInput('');
-  //     if(showIntro) setShowIntro(false);
-
-  //     setIsGenerating(true);
-
-      setTimeout(() => {
-        const botMessage = { text: 'Title: Certificates and solemnization of marriages upon alteration of divisions\n Law sections and its subsections related"<br>" to question: Section 4 ([8, 22 of 1955]), Section 10 ([10, 22 of 1946]), Section 14 ([7, 34 of 1946]) and Section 28 ([7, of 1944])\n Answer: For a marriage certificate to be issued by a district registrar from the old or new division when an area undergoes a transition as outlined in Law [7, of 1944] Section 28, a marriage must be solemnized in pursuance of Section 33 of the law without any of the preliminaries prescribed by Sections 4 and 10. The required acts must be done by a District Registrar of the old division or the new division nominated by the District Registrar within the District, and the Registrar-General must periodically publish a list of Registrars of Marriages in Sri Lanka, and the buildings they administer, as laid down by Section 14 ([7, 34 of 1946]).\n Conclusion: In conclusion', isUser: false };
-        const updatedMessagesWithBot = [...updatedMessages, botMessage];
-        localStorage.setItem('chatMessages', JSON.stringify(updatedMessagesWithBot));
-        setMessages(updatedMessagesWithBot);
-        setIsGenerating(false);
-      }, 500);
-      setIsGenerating(false);
-    }
-  };
-
-
-
   const clearChat = () => {
-    if(isGenerating) setIsGenerating(false);
+    if (isGenerating) setIsGenerating(false);
     localStorage.removeItem('chatMessages');
     setMessages([]);
-    if(!showIntro) setShowIntro(true);
+    if (!showIntro) setShowIntro(true);
   };
 
   return (
     <div className="chat-container">
       <div className="messages">
-
-      {showIntro && (
-        <div className="intro">
-          <img src={logo} alt="Logo" />
-          <div className="intro-header">      
-            <p className="initial">{initialMessage.text}</p>
-          </div>
-          <div className="intro-text">
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-              {prompts.map((prompt, index) => (
-                <div key={index} className="col">
-                  <div className="intro-card">
-                    <div className="d-flex align-items-center justify-content-between mb-2">
-                      {/* <img src="your-icon-path" alt="Icon" className="card-icon" /> */}
-                      <div className="card-body text-white">{prompt}</div>
-                      <div className="send-icon">
-                        <VscSend className="send-icon" />
+        {showIntro && (
+          <div className="intro">
+            <img src={logo} alt="Logo" />
+            <div className="intro-header">      
+              <p className="initial">{initialMessage.text}</p>
+            </div>
+            <div className="intro-text">
+              <div className="row row-cols-1 row-cols-md-2 g-4">
+                {prompts.map((prompt, index) => (
+                  <div key={index} className="col">
+                    <div className="intro-card">
+                      <div className="d-flex align-items-center justify-content-between mb-2">
+                        <div className="card-body text-white">{prompt}</div>
+                        <div className="send-icon">
+                          <VscSend className="send-icon" />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      )}
-
-
-
+        )}
         {messages.map((message, index) => (
           <div key={index} className={message.isUser ? 'user-message' : 'bot-message'}>
             {message.isUser ? (
@@ -173,28 +127,15 @@ const ChatBot = () => {
           onChange={handleUserInput}
           placeholder="Type your message..."
         />
-        {/* <Input 
-          type="text" 
-          variant="bordered" 
-          value={userInput}
-          onChange={handleUserInput}
-          placeholder="Type your message..." 
-        /> */}
-        <Button className="p-2" style={{width: '60px'}}isIconOnly color="primary" type="submit" aria-label="Like">
+        <button className="p-2" style={{width: '60px'}} aria-label="Send">
           <VscSend color="white" size="25 px" />
-        </Button>
-
-        <Button onClick={clearChat} color="error" variant="text" className="refresh-button" style={{ fontSize: '16px' }}>
-            <VscRefresh size={30} />
-        </Button>
-
+        </button>
+        <button onClick={clearChat} className="refresh-button" style={{ fontSize: '16px' }} aria-label="Clear">
+          <VscRefresh size={30} />
+        </button>
       </form>
-      
-      {/* <Button color="primary" variant="ghost" className="clear-chat" onClick={clearChat}>
-        New Chat 
-      </Button> */}
     </div>
   );
-};
+}
 
 export default ChatBot;
